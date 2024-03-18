ad6a5aac526a1706abb125a6cf179e3d30aac6e8
Leonardo Miguel J. Alejandro <94476994+Godllyra@users.noreply.github.com>
2024-03-18 12:39:15 -0600

    Suma, Resta, Multiplicación y División

---
+++ LengProg1/app2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+int main(int argc, char const *argv[])
+{
+    float a, b;
+    std::cout << "INGRESA EL VALOR DEL PRIMER NUMERO: ";
+    std::cin >> a;
+    std::cout << "INGRESA EL VALOR DEL SEGUNDO NUMERO: ";
+    std::cin >> b;
+
+    std::cout << "\nLa suma es: " << a + b << std::endl;
+    std::cout << "La resta es: " << a - b << std::endl;
+    std::cout << "La multiplicacion es: " << a * b <<std::endl;
+    std::cout << "La division es: " << a / b << std::endl;
+    return 0;
+}
