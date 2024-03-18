27453838da0effd5ac030fe3f9b30ab72f509947
Leonardo Miguel J. Alejandro <94476994+Godllyra@users.noreply.github.com>
2024-03-18 17:52:57 -0600

    Suma, Resta, Multiplicación y División

--- LengProg1/app2.cpp
+++ LengProg1/app2.cpp
@@ -10,6 +10,12 @@ int main(int argc, char const *argv[])
     std::cout << "\nLa suma es: " << a + b << std::endl;
     std::cout << "La resta es: " << a - b << std::endl;
     std::cout << "La multiplicacion es: " << a * b <<std::endl;
-    std::cout << "La division es: " << a / b << std::endl;
+    if (b == 0)
+    {
+        std::cout << "Division por cero. Sin solucion";
+    }else
+    {
+        std::cout << "La division es: " << a / b << std::endl;
+    }
     return 0;
 }
