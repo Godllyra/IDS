8014df0ac72dee703c817eb53ae58d6f98ba3647
Leonardo Miguel J. Alejandro <94476994+Godllyra@users.noreply.github.com>
2024-03-29 23:45:18 -0600

    Aplicación para Cálculo de RFC

--- LengProg1/app3.cpp
+++ LengProg1/app3.cpp
@@ -1,56 +1,65 @@
 #include <iostream>
+#include <string>
+#include <cctype>
+
+bool esVocal(char letra)
+{
+    letra = std::tolower(letra);
+    return (letra == 'a' || letra == 'e' || letra == 'i' || letra == 'o' || letra == 'u');
+}
+
+char getPrimeraVolcal(const std::string &apellido_p)
+{
+    for (char letra : apellido_p)
+    {
+        if (esVocal(letra))
+        {
+            return letra;
+        }
+    }
+    return 'X';
+}
+
+std::string calcRFC(const std::string &nombre, const std::string &apellido_p, const std::string &apellido_m, const std::string &nacimiento)
+{
+    std::string rfc;
+
+    char primerLetra_p = apellido_p[0];
+    char primerVocal = getPrimeraVolcal(apellido_p);
+
+    char primeraLetra_m = (!apellido_m.empty()) ? apellido_m[0] : 'x';
+    char primeraLetra_n = nombre[0];
+
+    std::string anio = nacimiento.substr(2, 2);
+    std::string mes = nacimiento.substr(5, 2);
+    std::string dia = nacimiento.substr(8, 2);
+
+    rfc = primerLetra_p;
+    rfc += primerVocal;
+    rfc += primeraLetra_m;
+    rfc += primeraLetra_n;
+    rfc += anio;
+    rfc += mes;
+    rfc += dia;
+    return rfc;
+}
 int main(int argc, char const *argv[])
 {
-    char rfc [10] = {'\0'};
-    char vocals [2] = {'\0'};
-    std::string apellido_p, apellido_m, nombre, dia, mes, anio;
-    std::cout << "INGRESA TU APELLIDO PATERNO: ";
-    std::getline(std::cin, apellido_p);
-    std::cout << "INGRESA TU APELLIDO MATERNO: ";
-    std::getline(std::cin, apellido_m);
-    std::cout << "INGRESA TU NOMBRE: ";
+    std::string nombre, apellido_p, apellido_m, nacimiento;
+    std::cout << "INGRESA TU NOMBRE> ";
     std::getline(std::cin, nombre);
-    std::cout << "FECHA DE NACIMIENTO (solo numero)\nDIA: ";
-    std::cin >> dia;
-    std::cout << "MES";
-    std::cin >> mes;
-    std::cout << "ANIO";
-    std::cin >> anio;
-    int j = 0;
-    for (size_t i = 0; i < apellido_p.length(); i++)
-    {   
-        if (j > 1)
-        {
-            i = apellido_p.length();
-        }
-        if (apellido_p[i] == 'a' || apellido_p[i] == 'e' || apellido_p[i] == 'i' || apellido_p[i] == 'o' || apellido_p[i] == 'u')
-        {
-            vocals[j] = apellido_p[i];
-            j++;   
-        }
-    }
-    if (apellido_p[0] == 'a' || apellido_p[0] == 'e' || apellido_p[0] == 'i' || apellido_p[0] == 'o' || apellido_p[0] == 'u')
-    {
-        for (size_t i = 0; i < 2; i++)
-        {
-            rfc[i] = vocals[i];
-        }
-    }else
-    {
-        rfc[0] = apellido_p[0];
-        rfc[1] = vocals[0];
-    }
-    rfc[2] = apellido_m[0];
-    rfc[3] = nombre[0];
-    rfc[4] = anio[0];
-    rfc[5] = anio[1];
-    rfc[6] = mes[0];
-    rfc[7] = mes[1];
-    rfc[8] = dia[0];
-    rfc[9] = dia[1];
-    for (size_t i = 0; i < 10; i++)
-    {
-        std::cout << rfc[i];
-    }
+
+    std::cout << "INGRESA TU APILLIDO PATERNO> ";
+    std::getline(std::cin, apellido_p);
+
+    std::cout << "INGRESA TU APELLIDO MATERNO (presiona <ENTER> si no tienes)> ";
+    std::getline(std::cin, apellido_m);
+
+    std::cout << "INGRESA TU FECHA DE NACIMIENTO EN EL SIGUIENTE FORMATO: YYYY-MM-DD> ";
+    std::getline(std::cin, nacimiento);
+
+    std::string RFC = calcRFC(nombre, apellido_p, apellido_m, nacimiento);
+
+    std::cout << "RFC: " << RFC << std::endl;
     return 0;
 }
